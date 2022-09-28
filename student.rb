6d0a7259fb99150dabfa39a42330a33e830dfb84
nimplay <nimrod7day@gmail.com>
2022-09-28 16:58:16 -0400

    fixing linters

--- student.rb
+++ student.rb
@@ -1,6 +1,7 @@
 require './person'
 class Student < Person
   attr_reader :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true, id: nil)
     super(age, name, parent_permission: parent_permission, id: id)
     @classroom = classroom
